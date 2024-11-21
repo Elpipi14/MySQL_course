327638d36337049ed0fda4c6dc0c5cd09d0782e2
Piuzzi A <ands_riss@hotmail.com>
2024-11-21 09:51:38 -0300

    2da Entrega

--- db_script_vfst.sql
+++ db_script_vfst.sql
@@ -1,7 +1,7 @@
-------------------------------------------------------------------------------------
------------------------------- Trigger ---------------------------------------------
+-- ----------------------------------------------------------------------------------
+-- ---------------------------- Trigger ---------------------------------------------
 
---Facturacion cuando el cliente cuando hace el pago:
+-- Facturacion cuando el cliente cuando hace el pago:
 
     -- Funcionalidad:
         -- Se ejecuta después de que se inserta un nuevo registro en la tabla pagos.
@@ -35,9 +35,11 @@ DELIMITER $$
     END$$
 DELIMITER ;
 
-------------------------------------------------------------------------------------
+select * from facturacion;
 
---Registra el historial cuando el cliente hace la reserva:
+-- ----------------------------------------------------------------------------------
+
+-- --Registra el historial cuando el cliente hace la reserva:
 
     -- Funcionalidad:
         -- Se ejecuta después de que se inserta un nuevo registro en la tabla reservas.
@@ -60,20 +62,22 @@ DELIMITER $$
         INSERT INTO historial_reservas (id_usuario, id_horario, fecha_reserva)
         VALUES (NEW.id_usuario, NEW.id_horario, NEW.fecha_reserva);
     END$$
-DELIMITER;
+DELIMITER ;
 
---------------------------------------------------------------------------------
---------------------------- Listado de Vistas ----------------------------------
+select * from historial_reservas
+
+-- ------------------------------------------------------------------------------
+-- ------------------------- Listado de Vistas ----------------------------------
 
 --  1. Vista: vista_usuarios_activos
 --  Descripción:
 --  Esta vista muestra una lista de usuarios activos junto con su tipo de membresía, el número de pagos realizados y la última fecha de pago. Es útil para monitorear a los clientes activos en el gimnasio.
 
---Objetivo:
---Identificar usuarios que están activos en el sistema.
---Facilitar la gestión de usuarios por parte del personal administrativo.
---Tablas Compuestas:
---usuarios
+-- Objetivo:
+-- Identificar usuarios que están activos en el sistema.
+-- Facilitar la gestión de usuarios por parte del personal administrativo.
+-- Tablas Compuestas:
+-- usuarios
 
 CREATE VIEW vista_usuarios_activos AS
 SELECT 
@@ -89,7 +93,7 @@ GROUP BY u.id_usuario, m.tipo_membresia;
 
 SELECT * FROM vista_usuarios_activos;
 
------------------------------------------------------------
+-- ---------------------------------------------------------
 
 -- 2. Vista: vista_clases_horarios
 -- Descripción:
@@ -151,7 +155,7 @@ INNER JOIN clases c ON h.id_clase = c.id_clase;
 
 SELECT * FROM vista_reservas_por_usuario;
 
------------------------------------------------------------
+-- ---------------------------------------------------------
 -- 4. Vista: vista_facturacion_mensual
 -- Descripción:
 -- Proporciona un resumen de facturación mensual, mostrando el total recaudado, el número de facturas emitidas y las membresías asociadas.
@@ -178,7 +182,7 @@ GROUP BY DATE_FORMAT(f.fecha_generacion, '%Y-%m');
 
 SELECT * FROM vista_facturacion_mensual;
 
------------------------------------------------------------
+-- ---------------------------------------------------------
 
 -- 5. Vista: vista_uso_membresias
 -- Descripción:
@@ -202,138 +206,45 @@ LEFT JOIN usuarios u ON m.id_membresia = u.id_membresia
 LEFT JOIN reservas r ON u.id_usuario = r.id_usuario
 GROUP BY m.tipo_membresia;
 
---------------------------------------------------------------------------------
-------------------------  Listado de Funciones ---------------------------------
-
--- 1. Función: cantidad_tipo_membresia_por_usuario
--- Descripción:
--- Calcula la cantidad de usuarios para cada tipo de membresía.
-
--- Objetivo:
--- Obtener una visión general del número de usuarios inscritos en cada tipo de membresía.
--- Ayudar a identificar las membresías más populares.
-
-DELIMITER $$
-
-CREATE PROCEDURE cantidad_tipo_membresia_por_usuario()
-BEGIN
-    SELECT 
-        m.tipo_membresia,
-        COUNT(u.id_usuario) AS cantidad_usuarios
-    FROM membresias m
-    LEFT JOIN usuarios u ON m.id_membresia = u.id_membresia
-    GROUP BY m.tipo_membresia;
-END$$
-
-DELIMITER ;
-
-CALL cantidad_tipo_membresia_por_usuario();
-
---------------------------------------------------------------------------------
-((REVISAR DE ACA PARA ABJO PARA TERMINAR LA ENTREGA))
--- 2. Función: cantidad_pagos_por_cliente
--- Descripción:
--- Calcula la cantidad de pagos realizados por cada cliente.
-
--- Objetivo:
--- Proporcionar un desglose del número de pagos realizados por cada cliente.
--- Facilitar el monitoreo de la frecuencia de pago de los usuarios.
-
-DELIMITER $$
-CREATE FUNCTION cantidad_pagos_por_cliente()
-RETURNS TABLE(nombre_cliente VARCHAR(200), cantidad_pagos INT)
-DETERMINISTIC
-BEGIN
-    RETURN (
-        SELECT 
-            CONCAT(u.nombre, ' ', u.apellido) AS nombre_cliente,
-            COUNT(p.id_pago) AS cantidad_pagos
-        FROM usuarios u
-        LEFT JOIN pagos p ON u.id_usuario = p.id_usuario
-        GROUP BY u.id_usuario
-    );
-END$$
-
-DELIMITER ;
-
--- Uso de la Función:
-SELECT * FROM cantidad_pagos_por_cliente();
-
---------------------------------------------------------------------------------
-
--- 3. Función: facturacion_total_por_mes
--- Descripción:
--- Calcula el monto total facturado en cada mes.
-
--- Objetivo:
--- Proporcionar un resumen financiero mensual.
--- Ayudar a la gerencia a monitorear los ingresos mensuales.
-
-DELIMITER $$
-CREATE FUNCTION facturacion_total_por_mes()
-RETURNS TABLE(mes VARCHAR(7), total_facturado DECIMAL(10, 2))
-DETERMINISTIC
-BEGIN
-    RETURN (
-        SELECT 
-            DATE_FORMAT(f.fecha_generacion, '%Y-%m') AS mes,
-            SUM(p.monto) AS total_facturado
-        FROM facturacion f
-        INNER JOIN pagos p ON f.id_pago = p.id_pago
-        GROUP BY DATE_FORMAT(f.fecha_generacion, '%Y-%m')
-        ORDER BY mes;
-    );
-END$$
-DELIMITER ;
-
--- Uso de la Función:
-SELECT * FROM facturacion_total_por_mes();
+SELECT * FROM vista_uso_membresias;
 
 --------------------------------------------------------------------------------
 ---------------------------- Stored Procedure ----------------------------------
 
--- 1. Stored Procedure: sp_generar_factura
+-- 1. Stored Procedure: sp_agregar_reserva
 -- Descripción:
--- Genera una factura para un pago específico, calculando las fechas de inicio y fin de la membresía asociada.
-
+-- Este procedimiento almacena una nueva reserva para un usuario en una clase específica. También actualiza el historial de reservas automáticamente.
 -- Objetivo:
--- Automatizar la creación de facturas relacionadas con pagos de usuarios.
--- Calcular automáticamente las fechas de vigencia de la membresía.
+-- Automatizar el proceso de agregar una reserva para un usuario.
+-- Garantizar la consistencia al registrar simultáneamente la reserva y el historial.
 -- Beneficio:
--- Asegura que las facturas se generen de manera uniforme y consistente.
--- Reduce la complejidad de manejar las facturas manualmente.
+-- Simplifica la inserción de reservas desde la aplicación o interfaz de usuario.
+-- Reduce errores al manejar automáticamente las dependencias entre reservas y historial_reservas.
 -- Tablas Utilizadas:
--- facturacion
--- pagos
+-- reservas
+-- historial_reservas
 
 DELIMITER $$
-CREATE PROCEDURE sp_generar_factura(
-    IN p_id_pago INT
-)
-BEGIN
-    DECLARE fecha_inicio DATE;
-    DECLARE fecha_fin DATE;
-
-    -- Obtener la fecha del pago
-    SELECT DATE(fecha_pago)
-    INTO fecha_inicio
-    FROM pagos
-    WHERE id_pago = p_id_pago;
-
-    -- Calcular la fecha fin sumando 1 mes
-    SET fecha_fin = DATE_ADD(fecha_inicio, INTERVAL 1 MONTH);
-
-    -- Insertar la factura
-    INSERT INTO facturacion (id_pago, fecha_inicio, fecha_fin, fecha_generacion)
-    VALUES (p_id_pago, fecha_inicio, fecha_fin, CURDATE());
-END$$
-
+	CREATE PROCEDURE sp_agregar_reserva(
+		IN p_id_usuario INT,
+		IN p_id_horario INT,
+		IN p_fecha_reserva DATETIME,
+		IN p_fecha_clase DATETIME
+	)
+	BEGIN
+		-- Insertar nueva reserva
+		INSERT INTO reservas (id_usuario, id_horario, fecha_reserva, fecha_clase, estado_reserva)
+		VALUES (p_id_usuario, p_id_horario, p_fecha_reserva, p_fecha_clase, 'activa');
+		-- Insertar en historial de reservas
+		INSERT INTO historial_reservas (id_usuario, id_horario, fecha_reserva)
+		VALUES (p_id_usuario, p_id_horario, p_fecha_reserva);
+	END$$
 DELIMITER ;
 
 -- Uso del Procedimiento:
--- CALL sp_generar_factura(3);
+CALL sp_agregar_reserva(1, 4, NOW(), '2024-12-28 15:00:00');
 
-------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------
 
 -- 2. Stored Procedure: sp_reporte_facturacion_mensual
 -- Descripción:
@@ -350,32 +261,79 @@ DELIMITER ;
 -- pagos
 
 DELIMITER $$
-CREATE PROCEDURE sp_reporte_facturacion_mensual()
-BEGIN
-    SELECT 
-        DATE_FORMAT(f.fecha_generacion, '%Y-%m') AS mes,
-        COUNT(f.id_factura) AS total_facturas,
-        SUM(p.monto) AS total_facturado
-    FROM facturacion f
-    INNER JOIN pagos p ON f.id_pago = p.id_pago
-    GROUP BY DATE_FORMAT(f.fecha_generacion, '%Y-%m')
-    ORDER BY mes;
-END$$
+
+	CREATE PROCEDURE sp_reporte_facturacion_mensual()
+	BEGIN
+		SELECT 
+			DATE_FORMAT(f.fecha_generacion, '%Y-%m') AS mes,
+			COUNT(f.id_factura) AS total_facturas,
+			SUM(p.monto) AS total_facturado
+		FROM facturacion f
+		INNER JOIN pagos p ON f.id_pago = p.id_pago
+		GROUP BY DATE_FORMAT(f.fecha_generacion, '%Y-%m')
+		ORDER BY mes;
+	END$$
 
 DELIMITER ;
 
 -- Uso del Procedimiento:
 CALL sp_reporte_facturacion_mensual();
 
-------------------   Resumen de los Stored Procedure   ---------------------------
+-- ------------------------------------------------------------------------------
+-- ----------------------  Listado de Funciones ---------------------------------
 
--- sp_generar_factura:
-    -- Objetivo: Generar una factura para un pago específico.
-    -- Beneficio: Asegura la consistencia en la generación de facturas.
-    -- Tablas: facturacion, pagos.
+-- 1. Función: cantidad_pagos_por_cliente
+-- Descripción:
+-- Calcula la cantidad de pagos realizados por cada cliente.
+
+-- Objetivo:
+-- Proporcionar un desglose del número de pagos realizados por cada cliente.
+-- Facilitar el monitoreo de la frecuencia de pago de los usuarios.
+
+DELIMITER $$
+CREATE FUNCTION cantidad_pagos_usuario(usuario_id INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE total_pagos INT;
     
--- sp_reporte_facturacion_mensual:
-    -- Objetivo: Proveer un resumen mensual de facturación.
-    -- Beneficio: Simplifica la generación de reportes financieros.
-    -- Tablas: facturacion, pagos.
+    SELECT COUNT(*) INTO total_pagos
+    FROM pagos
+    WHERE id_usuario = usuario_id;
 
+    RETURN total_pagos;
+END$$
+DELIMITER ;
+
+-- Uso:
+-- Devuelve la cantidad de pagos realizados por el usuario con ID 1.
+SELECT cantidad_pagos_usuario(1);
+
+--------------------------------------------------------------------------------
+
+-- 2. Función: reservas_usuario
+-- Descripción:
+-- Devuelve el total de reservas realizadas por un usuario específico.
+
+-- Objetivo:
+-- Proporcionar un resumen de reserva de cada usuario.
+-- Ayudar a la gerencia a monitorear la cantidad de resrvas por usuario.
+
+DELIMITER $$
+	CREATE FUNCTION reservas_usuario(usuario_id INT)
+	RETURNS INT
+	DETERMINISTIC
+	BEGIN
+		DECLARE total_reservas INT;
+		
+		SELECT COUNT(*) INTO total_reservas
+		FROM reservas
+		WHERE id_usuario = usuario_id;
+
+		RETURN total_reservas;
+	END$$
+DELIMITER ;
+
+-- Uso:
+-- Devuelve el total de reservas realizadas por el usuario con ID 2.
+SELECT reservas_usuario(1); 
